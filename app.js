218f0dd0b701b4b5c58a3e5979b36b1d27003147
Michael Salas <michaelsalasalcedo15@gmail.com>
2023-08-17 15:44:41 -0500

    initial service

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}.`);
+});
